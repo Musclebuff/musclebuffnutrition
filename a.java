706b76a1d6f39f30ba8e66aeaae8132bed87e214
Bharat <bjoshi272@gamil.com>
2023-09-20 23:03:01 +0530

    added one line in a.

--- a.java
+++ a.java
@@ -2,5 +2,6 @@ package musclebuffnutrition;
 public class a{
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("hii dunia");
     }
 }
