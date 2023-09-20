875cad507cad29e3fb05c34914fd1a4ea2d79bc3
Bharat <bjoshi272@gamil.com>
2023-09-21 00:13:00 +0530

    added some new codes.

--- a.java
+++ a.java
@@ -1,8 +1,14 @@
 package musclebuffnutrition;
+
 public class a{
+    public static int sum(int a, int b){
+        return a+b;
+    }
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("hii dunia");
         System.out.println("happy anniversary");
-    }
-}
+        int x = 3;
+        int y = 4;
+        System.out.println(sum(x,y));
+    }}
